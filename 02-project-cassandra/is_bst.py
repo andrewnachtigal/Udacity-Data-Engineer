e3ddcdd2282333779280ea0ccdfab6a21226bb66
andrewnachtigal <ajnacht@gmail.com>
2020-04-21 19:35:58 -0700

    is-bst

---
+++ 02-project-cassandra/is_bst.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+"""Is it a binary search tree
+
+Test whether a binary search tree data structure is implemented correctly.
+
+Given a binary search tree with integers as its key. Test whether it is a
+correct binary search tree.
+
+
+"""
+
+import sys, threading
+
+sys.setrecursionlimit(10**7) # max depth of recursion
+threading.stack_size(2**25)  # new thread will get stack of such size
+
+def IsBinarySearchTree(tree):
+  # Implement correct algorithm here
+  return True
+
+
+def main():
+  nodes = int(sys.stdin.readline().strip())
+  tree = []
+  for i in range(nodes):
+    tree.append(list(map(int, sys.stdin.readline().strip().split())))
+  if IsBinarySearchTree(tree):
+    print("CORRECT")
+  else:
+    print("INCORRECT")
+
+threading.Thread(target=main).start()
